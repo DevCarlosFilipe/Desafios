c5f55e88b3f0fb2ff4a29f80d4164789001414fb
Carlos Filipe <lordcfprogramador@gmail.com>
2025-01-16 23:08:29 -0300

    Update script.js

--- desafioNumerosCentrais/script.js
+++ desafioNumerosCentrais/script.js
@@ -2,15 +2,24 @@ var output = $("#output");
 var range = $("#range");
 var numRange = $("#numRange");
 
-function resultString(){
-    return "1";
+function findSquareLC (val) {
+    calc = Number.isInteger(val);
+    console.log(val - 1 + calc)
+    return calc;
+}
+function resultString(val){
+    var string = findSquareLC(val);
+    return string;
 }
 
-output.text("1");
-range.keyup(function () {
-    numRange.text(range.val())
+
+// Ativação de função com redundância, uma para cada cenário de alteração de valor Ao Vivo
+range.mousemove(function () {
+    numRange.text(range.val());
+    output.html(resultString(range.val()));
 });
 
 range.change(function () {
-    numRange.text(range.val())
+    numRange.text(range.val());
+    output.html(resultString(range.val()));
 });
